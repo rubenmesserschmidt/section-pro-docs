e00cddf1af1762c524d177b19adfab8c51314f46
Ruben Messerschmidt <ruben.messerschmidt@outlook.com>
2026-06-23 17:10:42 +0200

    Add initial Sphinx documentation for Section Pro
    - Created HTML documentation files including index and search pages.
    - Added Sphinx inventory file for object references.
    - Implemented search functionality with JavaScript support.
    - Configured Sphinx build script for Windows.
    - Set up Sphinx configuration with project details and extensions.
    - Created initial index.rst file with content structure.

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Section Pro'
+copyright = '2026, Ruben Messerschmidt'
+author = 'Ruben Messerschmidt'
+release = '1.0.0'
+extensions = ["sphinx.ext.intersphinx"]
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+intersphinx_mapping = {
+    "blender": ("https://docs.blender.org/api/current/", None),
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. Section Pro documentation master file, created by
+   sphinx-quickstart on Tue Jun 23 15:44:23 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Section Pro documentation
+=========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
